1272d8c7a13dae2ff29a1353092b4cfa65746d62
xhkocatepe <bayramhakankocatepe@gmail.com>
2018-11-09 00:07:20 +0100

    hey

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
  
 
 
